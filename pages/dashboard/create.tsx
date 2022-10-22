791a2c24a6f8f5ee177e8df7abe99c4187f84e8e
D3Portillo <d3portillo@gmail.com>
2022-10-22 13:46:51 +0200

    feat(dashboard/): create doctor end view

---
+++ pages/dashboard/create.tsx
@@ -0,0 +1,81 @@
+import { Fragment } from "react"
+import Head from "next/head"
+import Image from "next/image"
+import Input from "@/components/Input"
+import Button from "@/components/Button"
+import Layout from "@/components/Layout"
+
+import { CiPill } from "react-icons/ci"
+import { MdOutlineMedicalServices, MdOutlineSick } from "react-icons/md"
+import { BiCommentDetail } from "react-icons/bi"
+import { FaFileMedicalAlt } from "react-icons/fa"
+import { BsFileEarmarkMedical } from "react-icons/bs"
+import Header from "@/components/Header"
+
+function Logo() {
+  return (
+    <div className="w-40">
+      <Image
+        src="/logo.png"
+        width={198}
+        height={49}
+        alt=""
+        layout="responsive"
+      />
+    </div>
+  )
+}
+
+export default function Dashboard() {
+  return (
+    <Fragment>
+      <Head>
+        <title>Historik</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout className="mb-12" noFooter noBackButton>
+        <Logo />
+        <div className="mt-4" />
+        <section className="pb-2">
+          <Header>Create a new record</Header>
+          <p className="text-sm text-zinc-500">
+            There{"'"}s is a time limit of an hour to create/update this record.
+          </p>
+        </section>
+        <FormInput placeholder="Input Symptoms" Icon={MdOutlineSick}>
+          Symptoms
+        </FormInput>
+        <FormInput placeholder="Input Diagnosis" Icon={BsFileEarmarkMedical}>
+          Diagnosis
+        </FormInput>
+        <FormInput placeholder="Input Comments" Icon={BiCommentDetail}>
+          Comments
+        </FormInput>
+
+        <FormInput
+          placeholder="Input Treatment"
+          Icon={MdOutlineMedicalServices}
+        >
+          Treatment
+        </FormInput>
+
+        <FormInput placeholder="Input Medication" Icon={CiPill}>
+          Medication
+        </FormInput>
+        <Button>SEND</Button>
+      </Layout>
+    </Fragment>
+  )
+}
+
+function FormInput({ children, placeholder, Icon }) {
+  return (
+    <Fragment>
+      <div className="flex w-full items-center space-x-2">
+        <Icon className="text-2xl" />
+        <strong>{children}</strong>
+      </div>
+      <Input placeholder={placeholder} />
+    </Fragment>
+  )
+}
---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,39 @@
+import { Fragment } from "react"
+import Head from "next/head"
+import Image from "next/image"
+import Input from "@/components/Input"
+import Button from "@/components/Button"
+import Layout from "@/components/Layout"
+
+function Logo() {
+  return (
+    <div className="w-40">
+      <Image
+        src="/logo.png"
+        width={198}
+        height={49}
+        alt=""
+        layout="responsive"
+      />
+    </div>
+  )
+}
+
+export default function Dashboard() {
+  return (
+    <Fragment>
+      <Head>
+        <title>Historik</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout className="items-center justify-center" noFooter noBackButton>
+        <Logo />
+        <div className="mt-4" />
+        <Input placeholder="Input your phone number or DNI" />
+        <Button isLink href="/dashboard/create">
+          LOGIN
+        </Button>
+      </Layout>
+    </Fragment>
+  )
+}
